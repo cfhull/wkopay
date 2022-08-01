e82433f0bb6ff0a4eaebd6806d79712094401c83
Chris Hull <chris.f.hull@gmail.com>
2022-07-31 20:28:17 -0500

    fix: import fixes

--- src/remotion/MusicVideo/Pokemon/index.tsx
+++ src/remotion/MusicVideo/Pokemon/index.tsx
@@ -1,30 +1,14 @@
-import { findLastIndex } from 'lodash';
 import { Img } from 'remotion';
-import { useCurrentFrame, useVideoConfig } from 'remotion';
-import data from '../lyrics';
 import styles from './pokemon.module.css';
 
 type Props = {
   src?: string;
 };
 
-const LINE_OFFSET = 0.5;
-
 const Pokemon = ({ src }: Props) => {
-  const frame = useCurrentFrame();
-  const { fps } = useVideoConfig();
-
-  const lineIndex = findLastIndex(
-    data,
-    ({ startTime }: { startTime: number }) =>
-      startTime - LINE_OFFSET < frame / fps
-  );
-
-  const line = lineIndex < 0 ? '' : data[lineIndex]?.pokemon;
-
-  return line ? (
+  return src ? (
     <div className={styles.pokemon}>
-      <Img src={`/assets/images/${src}`} />
+      <Img src={require(`/assets/images/${src}`)} />
     </div>
   ) : null;
 };
