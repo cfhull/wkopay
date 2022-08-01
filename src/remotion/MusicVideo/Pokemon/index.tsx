09780021d62db69ded1d0145d76f1dce868cb2f1
Chris Hull <chris.f.hull@gmail.com>
2022-07-31 20:46:49 -0500

    fix: fancy asset loading

--- src/remotion/MusicVideo/Pokemon/index.tsx
+++ src/remotion/MusicVideo/Pokemon/index.tsx
@@ -1,7 +1,7 @@
 import { Img } from 'remotion';
 import styles from './pokemon.module.css';
 
-const images = import.meta.glob('/assets/*.png', { eager: true });
+const images: any = import.meta.glob('/assets/*.png', { eager: true });
 
 type Props = {
   src?: string;
@@ -10,7 +10,7 @@ type Props = {
 const Pokemon = ({ src }: Props) => {
   return src ? (
     <div className={styles.pokemon}>
-      <Img src={images.find((x) => x.endsWidth(src))} />
+      <Img src={images.find((x: string) => x.endsWidth(src))} />
     </div>
   ) : null;
 };
